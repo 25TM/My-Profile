52054d22114204fc83d091a3fddc40b3c6f92883
25TM <110965697+25TM@users.noreply.github.com>
2024-08-31 22:20:33 +0530

    Add files via upload

---
+++ MY portfolio/main.js
@@ -0,0 +1,7 @@
+var typed =new Typed(".text",{
+    strings:["Web Developer","Aspiring Machine Learning and AI Engineer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
